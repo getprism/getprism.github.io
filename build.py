066f968c4587d0faa3fe795c39f8f82910642573
Lachlan Charlick <lachlan.charlick@gmail.com>
2018-06-21 21:17:03 +0930

    Set version number on index.html

--- build.py
+++ build.py
@@ -3,6 +3,8 @@ import glob
 import markdown2
 import jinja2
 
+from typing import List
+
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
 
@@ -10,20 +12,40 @@ def f(name: str) -> str:
     return os.path.join(BASE_DIR, name)
 
 
+def release_files() -> List[str]:
+    return sorted(glob.glob(os.path.join(BASE_DIR, 'releases', '*.md')), reverse=True)
+
+
+def current_release(releases: List[str]) -> str:
+    names = [release_name(release) for release in releases]
+    released = [release for release in names if 'release' in release]
+    return released[0] if len(released) > 0 else names[-1]
+
+
+def release_name(release: str) -> str:
+    return os.path.splitext(os.path.basename(release))[0]
+
+
+def release_version(release: str) -> str:
+    return release[:5]
+
+
 with open(f('index.template.html'), 'r') as template_source, \
         open(f('index.html'), 'w') as output:
     template = jinja2.Template(template_source.read())
-    output.write(template.render(version='1.0.0'))
+    version = release_version(current_release(release_files()))
+    if version[-2:] == '.0':
+        version = version[:-2]
+    output.write(template.render(version=version))
 
 with open(f('releases.template.html'), 'r') as template_source, \
         open(f('releases.html'), 'w') as output:
-    release_files = glob.glob(os.path.join(BASE_DIR, 'releases', '*.md'))
     releases = []
-    for release_file in sorted(release_files, reverse=True):
-        file_name = os.path.splitext(os.path.basename(release_file))[0]
-        location = file_name[6:]
+    for release_file in release_files():
+        name = release_name(release_file)
+        location = name[6:]
         releases.append({
-            'version': file_name[:5],
+            'version': release_version(name),
             'location': location,
             'content': markdown2.markdown(open(release_file).read()),
         })
