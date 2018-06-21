600b6c1fa627b4c57e996b3faa31e73b7234231f
Lachlan Charlick <lachlan.charlick@gmail.com>
2018-06-21 19:51:25 +0930

    Basic release page generation

--- build.py
+++ build.py
@@ -1,13 +1,30 @@
-import jinja2
 import os.path
+import glob
+import markdown2
+import jinja2
+
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
 
 def f(name: str) -> str:
-    base_dir = os.path.dirname(os.path.realpath(__file__))
-    return os.path.join(base_dir, name)
+    return os.path.join(BASE_DIR, name)
 
 
 with open(f('index.template.html'), 'r') as template_source, \
-        open(f('index.html'), 'w') as index:
+        open(f('index.html'), 'w') as output:
     template = jinja2.Template(template_source.read())
-    index.write(template.render(version='1.0.0'))
+    output.write(template.render(version='1.0.0'))
+
+with open(f('releases.template.html'), 'r') as template_source, \
+        open(f('releases.html'), 'w') as output:
+    release_files = glob.glob(os.path.join(BASE_DIR, 'releases', '*.md'))
+    releases = []
+    for release_file in release_files:
+        file_name = os.path.splitext(os.path.basename(release_file))[0]
+        releases.append({
+            'version': file_name,
+            'content': markdown2.markdown(open(release_file).read()),
+        })
+    # releases = [os.path.splitext(os.path.basename(fname))[0] for fname in release_files]
+    template = jinja2.Template(template_source.read())
+    output.write(template.render(releases=releases))
